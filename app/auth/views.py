7dcc4528ac349ee9fe2b511aab839094f71fc0d4
SleenXiu <xiushilin@hotmail.com>
2016-04-22 13:29:23 +0800

    用户资料
    用户资料显示、修改、管理员的资料修改、用户头像

--- app/auth/views.py
+++ app/auth/views.py
@@ -54,8 +54,9 @@ def register():
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated \
-        and not current_user.confirmed \
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
         and request.endpoint[:5] != 'auth.' \
         and request.endpoint != 'static':
             return redirect(url_for('auth.unconfirmed'))
--- app/models.py
+++ app/models.py
@@ -8,8 +8,9 @@ from . import db
 from . import login_manager
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 import hashlib
+from flask_moment import datetime
 
 
 # 用户角色
@@ -63,7 +64,12 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean,default=False)
-
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
     # 构造函数——初始化角色
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -72,6 +78,10 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(permissions=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+
+
     # 角色权限验证
     def can(self, permissions):
         return self.role is not None and\
@@ -148,6 +158,23 @@ class User(UserMixin, db.Model):
             self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
+
+    # 刷新最后访问时间
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
+    # 使用gravatar设置头像
+    def gravatar(self, size=100, default='identicon', rating='pg'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(
+            self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
